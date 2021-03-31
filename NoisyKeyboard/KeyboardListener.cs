a855d0f8d960303571535b1048c76f0fad09095c
Prashanth Chamegowda <ME_a_PBANGAL@mercedesme.onmicrosoft.com>
2021-03-31 17:45:17 +0200

    Initial commit

---
+++ NoisyKeyboard/KeyboardListener.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Media;
+using System.IO;
+
+namespace NoisyKeyboard
+{
+    class KeyboardListener : IDisposable
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private bool disposedValue;
+        private static Stack<MediaPlayer> _mediaPlayers = new Stack<MediaPlayer>();
+        private static double _volume = 10;
+        private static string _soundFolder;
+        public static void SetVolume(double volume)
+        {
+            if(volume < 10)
+            { volume = 10; }
+            _volume = volume;
+        }
+        public static void SetMusicFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                _soundFolder = folderPath;
+                Init();
+            }
+
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        
+
+        static KeyboardListener()
+        {
+            Init();
+        }
+
+        private static void Init()
+        {
+            if (String.IsNullOrEmpty(_soundFolder))
+            {
+                _soundFolder =  $"{Directory.GetCurrentDirectory()}\\data\\sounds\\mechanical"; 
+            }
+
+            _mediaPlayers.Clear();
+            for (int i = 0; i < 20; i++)
+            {
+                var mediaPlayer = new MediaPlayer();
+                Directory.GetCurrentDirectory();
+                mediaPlayer.Open(new Uri($"{_soundFolder}\\1.wav"));
+                _mediaPlayers.Push(mediaPlayer);
+            }
+        }
+
+        private static IntPtr HookCallback(
+        int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                //Call the music play here
+                new Thread(PlaySound).Start();
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        private static void PlaySound()
+        {
+            var mediaPlayer = _mediaPlayers.Pop();
+            mediaPlayer.Dispatcher.Invoke(() => {
+                mediaPlayer.Stop();
+                mediaPlayer.Volume = _volume / 100.0;
+                mediaPlayer.Play();
+                _mediaPlayers.Push(mediaPlayer);
+            });
+            
+            
+
+        }
+        public static void StartHook()
+        {
+            if(_hookID == IntPtr.Zero)
+            {
+                _hookID = SetHook(_proc);
+            }
+           
+
+        }
+        public static void StopHook()
+        {
+            if (_hookID != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+            _hookID = IntPtr.Zero;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    KeyboardListener.StopHook();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~KeyboardListener()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ NoisyKeyboard/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace NoisyKeyboard
+{
+    
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool forceExit = false;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            var soundDirectories = Directory.EnumerateDirectories($"{Directory.GetCurrentDirectory()}\\data\\sounds");
+            foreach(var folder in soundDirectories)
+            {
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                var contentText = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(folder.Split('\\').Last().ToLower());
+                
+                comboBoxItem.Content = contentText;
+                comboBoxItem.Tag = folder;
+                comboboxSounds.Items.Add(comboBoxItem);
+            }
+            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Top = System.Windows.SystemParameters.PrimaryScreenHeight - this.Height - 30;
+            comboboxSounds.SelectedIndex = 0;
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!forceExit)
+            {
+                e.Cancel = true;
+                this.WindowState = WindowState.Minimized;
+            }
+        }
+        private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            KeyboardListener.SetVolume(e.NewValue);
+        }
+
+        private void comboboxSounds_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            KeyboardListener.SetMusicFolder((String)((ComboBoxItem)comboboxSounds.SelectedItem).Tag);
+        }
+
+        private void btnCheckEnableSound_Checked(object sender, RoutedEventArgs e)
+        {
+            KeyboardListener.StartHook();
+        }
+
+        private void btnCheckEnableSound_Unchecked(object sender, RoutedEventArgs e)
+        {
+            KeyboardListener.StopHook();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            forceExit = true;
+            this.Close();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
